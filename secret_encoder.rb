9669e3b4cce78f9c4b76a0d934e6426189538cc1
yue-cherry-ying <cherryying@uchicago.edu>
2020-10-16 21:20:01 +0000

    working on encoder

--- secret_encoder.rb
+++ secret_encoder.rb
@@ -6,3 +6,16 @@
 # Your program should print the encoded message.
 
 p "Enter in the secret you want to encode"
+message = gets.chomp.split
+# .gsub(/\s+/, "").split("")
+p message
+new_words = []
+message.each do |word|
+  wt_a = word.downcase.gsub("a", "1")
+  wt_e = wt_a.gsub("e", "2")
+  wt_i = wt_e.gsub("i", "3")
+  wt_o = wt_i.gsub("o", "4")
+  wt_u = wt_o.gsub("u", "5")
+  new_words.push(wt_u)
+end
+p new_words.join(" ")
