b8066cbb765330e01eabfbedb796a2899cb4a467
yue-cherry-ying <cherryying@uchicago.edu>
2020-10-16 21:47:55 +0000

    finished with secret_encoder

--- secret_encoder.rb
+++ secret_encoder.rb
@@ -11,11 +11,16 @@ message = gets.chomp.split
 p message
 new_words = []
 message.each do |word|
-  wt_a = word.downcase.gsub("a", "1")
+  wt_a = word.gsub("a", "1")
   wt_e = wt_a.gsub("e", "2")
   wt_i = wt_e.gsub("i", "3")
   wt_o = wt_i.gsub("o", "4")
   wt_u = wt_o.gsub("u", "5")
-  new_words.push(wt_u)
+  wt_A = wt_u.gsub("A", "1")
+  wt_E = wt_A.gsub("E", "2")
+  wt_I = wt_E.gsub("I", "3")
+  wt_O = wt_I.gsub("O", "4")
+  wt_U = wt_O.gsub("U", "5")
+  new_words.push(wt_U)
 end
 p new_words.join(" ")
