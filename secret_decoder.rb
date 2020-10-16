5c9492e9b141c37090bd72212f8a7ab41612397d
yue-cherry-ying <cherryying@uchicago.edu>
2020-10-16 22:28:45 +0000

    finished with secret_decoder

--- secret_decoder.rb
+++ secret_decoder.rb
@@ -8,3 +8,17 @@
 # (Make sure to capitalize the first word in the decoded message)
 
 p "Enter in the secret you want to decode"
+message = gets.chomp.split
+# .gsub(/\s+/, "").split("")
+p message
+new_words = []
+message.each do |word|
+  wt_a = word.gsub("1", "a")
+  wt_e = wt_a.gsub("2", "e")
+  wt_i = wt_e.gsub("3", "i")
+  wt_o = wt_i.gsub("4", "o")
+  wt_u = wt_o.gsub("5", "u")
+  new_words.push(wt_u)
+end
+
+p new_words.join(" ").capitalize
